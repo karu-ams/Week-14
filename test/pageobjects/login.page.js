754d024893da79b64730db769d8f2b11fccd8bd5
Karen Agustina Soto <karenagustinams@gmail.com>
2022-06-21 19:35:49 -0300

    BaSP-QA: Login automated test UPDATED

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,14 +1,12 @@
 class LoginPage {
     // Getters
-    get inputUsername () {
-        return $('#user-name');
-    }
-    get inputPassword () {
-        return $('#password');
-    }
-    get btnLogin () {
-        return $('#login-button');
-    }
+    get inputUsername () { return $('#user-name') }
+    get inputPassword () { return $('#password') }
+    get btnLogin () { return $('#login-button') }
+    get errorContainer () { return $('.error-message-container.error') }
+    get botImg () { return $('.bot_column') }
+    get loginLogo () { return $('.login_logo') }
+    get title() {return $('.title')}
     // Setters
     async setUsername(username) {
         await this.inputUsername.setValue(username);
@@ -17,6 +15,9 @@ class LoginPage {
         await this.inputPassword.setValue(password);
     };
     // Methods
+    // Open browser
+    open() { return browser.url('http://www.saucedemo.com/') }
+    // Log in method
     async login (username, password) {
         await this.setUsername(username);
         await this.setPassword(password);
@@ -24,4 +25,4 @@ class LoginPage {
     };
 };
 
-module.exports = new LoginPage();
+export default new LoginPage();
--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -1,10 +1,77 @@
-const LoginPage = require('../pageobjects/login.page');
+import LoginPage from '../pageobjects/login.page';
 
-describe('Login page testing', () => {
-    beforeAll('Open browser', async () => {
-        browser.url('https://www.saucedemo.com/');
+describe('Login test negative scenarios, with missing or invalid data', () => {
+    describe('All fields empty testing', () => {
+        it('Username and password fields are empty, should display an error', async () => {
+            await LoginPage.open()
+            await LoginPage.login('','')
+            await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Username is required')
+        })
     })
-    it('Login successfully completed', async () => {
-        await LoginPage.login('standart_user', 'secret_sauce');
-    });
+    describe('Username input testing', () => {
+        it('Empty username, should display an error', async () => {
+            await LoginPage.open()
+            await LoginPage.login('','secret_sauce')
+            await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Username is required')
+        })
+        it('Invalid username, should display an error', async () => {
+            await LoginPage.open()
+            await LoginPage.login('invalid','secret_sauce');
+            await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Username and password do not match any user in this service')
+        })
+    })
+    describe('Password input testing', () => {
+        it('Empty password, should display an error', async () => {
+            await LoginPage.open()
+            await LoginPage.login('standard_user','');
+            await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Password is required')
+        })
+        it('Invalid password, should display an error', async () => {
+            await LoginPage.open()
+            await LoginPage.login('standard_user','invalid');
+            await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Username and password do not match any user in this service')
+        })
+    })
+});
+
+describe('Login test possitive scenarios with valid credentials', () => {
+    describe('Using standard_user data', () => { 
+        it('Login should be successful', async () => {
+            await LoginPage.open()
+            await LoginPage.login('standard_user','secret_sauce');
+            await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html')
+        })
+    })
+    describe('Using locked_out_user data', () => { 
+        it('An error should be displayed, the user is locked out', async () => {
+            await LoginPage.open()
+            await LoginPage.login('locked_out_user','secret_sauce');
+            await expect(LoginPage.errorContainer).toHaveText('Epic sadface: Sorry, this user has been locked out.')
+        })
+    })
+    describe('Using problem_user data', () => { 
+        it('Login should be successful but the inventory page has problems', async () => {
+            await LoginPage.open()
+            await LoginPage.login('problem_user','secret_sauce');
+            await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html')
+        })    
+    })
+    describe('Using performance_glitch_user data', () => { 
+        it('Login should be successful', async () => {
+            await LoginPage.open()
+            await LoginPage.login('performance_glitch_user','secret_sauce');
+            await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html')
+        })
+    })  
+});
+
+describe('Elements to be displayed testing', () => {
+    it('Verify robot image to be displayed', async () => {
+        await LoginPage.open()
+        await expect(LoginPage.botImg).toBeDisplayed();
+    })
+    it('Verify logo image to be displayed', async () => {
+        await LoginPage.open()
+        await expect(LoginPage.loginLogo).toBeDisplayed();
+    })
 });
