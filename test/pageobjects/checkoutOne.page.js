44be7bc31b2e0e9671f264d346fdcfadd80915f1
Karen Agustina Soto <karenagustinams@gmail.com>
2022-06-22 14:29:06 -0300

    BaSP-QA: Checkout first step automated test added

--- test/pageobjects/checkoutOne.page.js
+++ test/pageobjects/checkoutOne.page.js
@@ -1,10 +1,9 @@
-
-class CheckoutOne {
+class CheckoutOnePage {
 
     // GETTERS
-    get firstNameInput () { return $('#first-name') }
-    get lastNameInput () { return $('#last-name') }
-    get postalCodeInput () { return $('#postal-code') }
+    get inputFirstName () { return $('#first-name') }
+    get inputLastName () { return $('#last-name') }
+    get inputPostalCode () { return $('#postal-code') }
 
     get cancelBtn () { return $('#cancel') }
     get continueBtn () { return $('#continue') }
@@ -12,18 +11,18 @@ class CheckoutOne {
 
     // SETTERS
     async setFirstName (firstName) {
-        await this.firstNameInput.setValue(firstName);
+        await this.inputFirstName.setValue(firstName);
     }
     async setLastName (lastName) {
-        await this.lastNameInput.setValue(lastName);
+        await this.inputLastName.setValue(lastName);
     }
     async setPostalCode (postalCode) {
-        await this.postalCodeInput.setValue(postalCode);
+        await this.inputPostalCode.setValue(postalCode);
     }
 
     // METHODS
     // Open browser
-    // openCheckoutOne() { return browser.url('https://www.saucedemo.com/checkout-step-one.html') }
+    // openCheckoutOnePage() { return browser.url('https://www.saucedemo.com/checkout-step-one.html') }
     // Checkout your information method
     async continueCheckout(firstName, lastName, postalCode) {
         await this.setFirstName(firstName)
@@ -31,6 +30,6 @@ class CheckoutOne {
         await this.setPostalCode(postalCode)
         await this.continueBtn.click()
     }
-}
+};
 
-export default new CheckoutOne();
+export default new CheckoutOnePage();
--- test/specs/checkoutOne.spec.js
+++ test/specs/checkoutOne.spec.js
@@ -1,7 +1,7 @@
 import LoginPage from '../pageobjects/login.page'
 import InventoryPage from "../pageobjects/inventory.page"
 import CartPage from '../pageobjects/cart.page'
-import CheckoutOne from '../pageobjects/checkoutOne.page'
+import CheckoutOnePage from '../pageobjects/checkoutOne.page'
 
 describe('Checkout information test', () => {
 
@@ -13,29 +13,28 @@ describe('Checkout information test', () => {
         InventoryPage.cartBtn.click()
         CartPage.checkoutBtn.click()
     });
-
     describe('Test with all information fields empty', () => {
         it('Should display an error message', async () => {
-            await CheckoutOne.continueCheckout('','','')
-            await expect(CheckoutOne.errorContainer).toHaveText('Error: First Name is required')
+            await CheckoutOnePage.continueCheckout('','','')
+            await expect(CheckoutOnePage.errorContainer).toHaveText('Error: First Name is required')
         })
     })
     describe('Test with some fields empty', () => {
         it('Empty name, should display an error', async () => {
-            await CheckoutOne.continueCheckout('','Soto','2000')
-            await expect(CheckoutOne.errorContainer).toHaveText('Error: First Name is required')
+            await CheckoutOnePage.continueCheckout('','Soto','2000')
+            await expect(CheckoutOnePage.errorContainer).toHaveText('Error: First Name is required')
         })
         it('Empty last name, should display an error', async () => {
-            await CheckoutOne.continueCheckout('Karen','','2000')
-            await expect(CheckoutOne.errorContainer).toHaveText('Error: Last Name is required')
+            await CheckoutOnePage.continueCheckout('Karen','','2000')
+            await expect(CheckoutOnePage.errorContainer).toHaveText('Error: Last Name is required')
         })
         it('Empty postal code, should display an error', async () => {
-            await CheckoutOne.continueCheckout('Karen','Soto','')
-            await expect(CheckoutOne.errorContainer).toHaveText('Error: Postal Code is required')
+            await CheckoutOnePage.continueCheckout('Karen','Soto','')
+            await expect(CheckoutOnePage.errorContainer).toHaveText('Error: Postal Code is required')
         })
     })
     describe('Test with valid data', () => {
-        CheckoutOne.continueCheckout('Karen','Soto','2000')
+        CheckoutOnePage.continueCheckout('Karen','Soto','2000')
         expect(browser).toHaveUrl('https://www.saucedemo.com/checkout-step-two.html')
     })
 });
