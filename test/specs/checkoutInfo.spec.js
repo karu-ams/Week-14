1acd9ba9b0209fe4b230e5c345696a1258c90c9d
Karen Agustina Soto <karenagustinams@gmail.com>
2022-06-22 20:17:52 -0300

    BaSP-QA: Checkout information page automated test modified.

---
+++ test/specs/checkoutInfo.spec.js
@@ -0,0 +1,52 @@
+import LoginPage from '../pageobjects/login.page'
+import InventoryPage from '../pageobjects/inventory.page'
+import CartPage from '../pageobjects/cart.page'
+import FirstCheckoutPage from '../pageobjects/firstCheckout.page'
+
+describe('Checkout information test NEGATIVE cases', () => {
+    beforeEach('Log in and add to cart', () => {
+        LoginPage.open()
+        LoginPage.login('standard_user','secret_sauce')
+        InventoryPage.addBoltShirtBtn.click()
+        InventoryPage.cartBtn.click()
+        CartPage.checkoutBtn.click()
+    })
+    describe('Test with all information fields empty', () => {
+        it('Should display an error message', async () => {
+            await FirstCheckoutPage.openFirstCheckoutPage()
+            await FirstCheckoutPage.continueCheckout('','','')
+            await expect(FirstCheckoutPage.checkoutErrorContainer).toHaveText('Error: First Name is required')
+        })
+    })
+    describe('Test with some fields empty', () => {
+        it('Empty name, should display an error', async () => {
+            await FirstCheckoutPage.openFirstCheckoutPage()
+            await FirstCheckoutPage.continueCheckout('','Soto','2000')
+            await expect(FirstCheckoutPage.checkoutErrorContainer).toHaveText('Error: First Name is required')
+        })
+        it('Empty last name, should display an error', async () => {
+            await FirstCheckoutPage.openFirstCheckoutPage()
+            await FirstCheckoutPage.continueCheckout('Karen','','2000')
+            await expect(FirstCheckoutPage.checkoutErrorContainer).toHaveText('Error: Last Name is required')
+        })
+        it('Empty postal code, should display an error', async () => {
+            await FirstCheckoutPage.openFirstCheckoutPage()
+            await FirstCheckoutPage.continueCheckout('Karen','Soto','')
+            await expect(FirstCheckoutPage.checkoutErrorContainer).toHaveText('Error: Postal Code is required')
+        })
+    })
+})
+
+describe('Checkout information test POSITIVE cases', () => {
+    beforeEach('Log in and add to cart', () => {
+        LoginPage.open()
+        LoginPage.login('standard_user','secret_sauce')
+        InventoryPage.addBoltShirtBtn.click()
+        InventoryPage.cartBtn.click()
+        CartPage.checkoutBtn.click()
+    })
+    it('Should redirect to next page', async () => {
+        await FirstCheckoutPage.continueCheckout('Karen','Soto','2000')
+        await expect(browser).toHaveUrl('https://www.saucedemo.com/checkout-step-two.html')
+    })
+})
